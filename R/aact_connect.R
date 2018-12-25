bb4e3c4628e820e39c6d44e07650cea159fabd27
jasonbaik94 <joonwoob@andrew.cmu.edu>
2018-12-25 16:01:30 -0500

    Edited aact_connect.R

--- R/aact_connect.R
+++ R/aact_connect.R
@@ -5,17 +5,21 @@
 #'database in the cloud using R without needing to install PostgreSQL on your
 #'local machine.
 #'
-#'@param user A character vector containing username.
-#'@param password A character vector containing password.
+#' First, create a config yaml file to securely save credentials: \code{yaml::write_yaml(data.frame(user = "USERNAME", password = "PASSWORD"), "config")}
+#' Then, load config file and save to an object: \code{config <- yaml::yaml.load_file("config")}
+#'
+#'@param user A character vector containing username: \code{config$user}
+#'@param password A character vector containing password: \code{config$password}
 
 #' @import RPostgreSQL
 #' @import DBI
+#' @import yaml
 #' @export
 #' @author Jason Baik <joonwoob@andrew.cmu.edu>
 #'
 #' @examples
 #' \dontrun{
-#' aact_connect("myid", "mypassword")
+#' aact_connect(config$user, config$password)
 #' }
 #'
 #' @seealso \url{https://aact.ctti-clinicaltrials.org/points_to_consider}
@@ -24,10 +28,11 @@ aact_connect <- function(user, password) {
 
   drv <- DBI::dbDriver('PostgreSQL')
   con <- DBI::dbConnect(drv,
-                   dbname="aact",
-                   host="aact-db.ctti-clinicaltrials.org",
-                   port=5432,
-                   user=user,
-                   password=password)
+                   dbname = "aact",
+                   host = "aact-db.ctti-clinicaltrials.org",
+                   port = 5432,
+                   user = user,
+                   password = password)
 
+  return(con)
 }
