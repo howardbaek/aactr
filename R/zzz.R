66182bd883ebc132ccd14b8896a9ceaf5c1a8fcc
howardbaik <howardhowiebaik@gmail.com>
2021-12-07 14:59:22 -0800

    Add package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to aactr")
+
+  startup_msg <- c(
+    "Welcome to aactr!",
+    "To use this package, please create your database account at this link:",
+    "https://aact.ctti-clinicaltrials.org/users/sign_up"
+  )
+
+  packageStartupMessage(paste(startup_msg, collapse = "\n"))
 }
