4f2df3dfca526ac706e7b5ae220ad98f78202514
howardbaik <howardhowiebaik@gmail.com>
2021-12-08 13:03:17 -0800

    Polish documentation for aact_connect()

--- R/aact_connect.R
+++ R/aact_connect.R
@@ -1,16 +1,20 @@
-#'Connect to AACT database.
+#' Establish a connection to AACT database
 #'
-#'According to \href{https://aact.ctti-clinicaltrials.org/r}{
-#' Connect to AACT using R}, this function allows you to access the PostgreSQL AACT
+#' This function allows you to access the PostgreSQL AACT
 #'database in the cloud using R without needing to install PostgreSQL on your
 #'local machine.
 #'
-#' First, create a config yaml file to securely save credentials: \code{yaml::write_yaml(data.frame(user = "USERNAME", password = "PASSWORD"), "config")}
-#' Then, load config file and save to an object: \code{config <- yaml::yaml.load_file("config")}
+#' To get started, please create a database account at this link: \cr
+#' \url{https://aact.ctti-clinicaltrials.org/users/sign_up}. \cr
+#' \cr
+#' To securely save credentials, create a config yaml file: \code{yaml::write_yaml(data.frame(user = "USERNAME", password = "PASSWORD"), "config").}
+#' Then, load the config file and save it to an object: \code{config <- yaml::yaml.load_file("config")}.
+#' Now, you can safely access your database credentials with \code{config$user} and \code{config$password}. \cr
 #'
-#'@param user A character vector containing username: \code{config$user}
-#'@param password A character vector containing password: \code{config$password}
-#'@param password A character vector containing password: \code{config$password}
+#' Alternatively, you can directly enter your username and password as arguments.
+#'
+#'@param user A character vector containing username of database account
+#'@param password A character vector containing password of database account
 
 #' @import RPostgreSQL
 #' @import DBI
@@ -20,10 +24,14 @@
 #'
 #' @examples
 #' \dontrun{
-#' aact_connect(config$user, config$password)
+#' con <- aact_connect(config$user, config$password)
 #' }
 #'
-#' @seealso \url{https://aact.ctti-clinicaltrials.org/points_to_consider}
+#' @seealso
+#' \itemize{
+#'   \item \url{https://aact.ctti-clinicaltrials.org/r}
+#'   \item \url{https://aact.ctti-clinicaltrials.org/points_to_consider}
+#' }
 
 aact_connect <- function(user, password) {
 
